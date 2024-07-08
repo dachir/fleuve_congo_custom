47547e9b967a9b2227434eb4c69aff83280fbc78
dachir <dodziamouzou@gmail.com>
2024-07-08 17:33:44 +0000

    080720241830

--- fleuve_congo_custom/fleuve_congo_custom/doctype/preparation_provision_decompte/preparation_provision_decompte.py
+++ fleuve_congo_custom/fleuve_congo_custom/doctype/preparation_provision_decompte/preparation_provision_decompte.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2024, Kossivi Amouzou and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class PreparationProvisionDecompte(Document):
